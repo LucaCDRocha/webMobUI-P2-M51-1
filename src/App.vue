b1a476cb62697f82e1de8a068ae27a91011ab817
LucaCDRocha <lucadrocha@gmail.com>
2024-04-09 11:40:10 +0200

    Add temperature conversion functionality to App.vue and create AppValue.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from "vue";
+import AppValue from "./components/AppValue.vue";
+
+const round = (val) => Math.round(val * 100) / 100;
+
+const tempSI = ref(273.15);
+
+const tempCelsius = computed({
+	get: () => round(tempSI.value - 273.15),
+	set: (val) => {
+		tempSI.value = val + 273.15;
+	},
+});
+
+const tempFahrenheit = computed({
+	get: () => round(((tempSI.value - 273.15) * 9) / 5 + 32),
+	set: (val) => {
+		tempSI.value = ((val - 32) * 5) / 9 + 273.15;
+	},
+});
+
+const tempKelvin = computed({
+	get: () => round(tempSI.value),
+	set: (val) => {
+		tempSI.value = val;
+	},
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div>
+		<h1>Temperature Converter</h1>
+		<AppValue label="Kelvin" v-model="tempKelvin" />
+		<AppValue label="Celsius" v-model="tempCelsius" />
+		<AppValue label="Fahrenheit" v-model="tempFahrenheit" />
+	</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+div {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 }
 </style>
---
+++ src/components/AppValue.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps(["label"]);
+const model = defineModel();
+</script>
+
+<template>
+	<div>
+		<label :for="label" >{{ label }}</label>
+		<input :id="label" type="number" v-model="model" />
+	</div>
+</template>
+
+<style scoped>
+div {
+	margin-top: 10px;
+}
+label {
+	font-weight: bold;
+	align-self: start;
+	margin: 10px 0px;
+}
+</style>
